dd9083ea665f220ca8505e9376c3ed1961aed3d4
Taiyab <taiyab0523@gmail.com>
2023-08-07 10:57:53 +0530

    WIP, Last updated 3 days ago, waiting for team support

--- script.js
+++ script.js
@@ -10,6 +10,54 @@ async function fetchingData () {
     const data = await response.json();
     console.log(data.products);
 
+    // Creating a logic function for rendering the cards
+    // It will be mapping filtered data
+    function renderCard(filteredData) {
+        
+        // we are going select our main body who is the parent of card structure
+        const mainBody = document.querySelector('.product-container');
+        console.log(mainBody, "This is main body.");
+
+        // 
+        mainBody.innerHTML = '';
+
+        filteredData.forEach((element) => {
+            mainBody.innerHTML += ` <div class="card" style="height: 30%; width: 30%;">
+            <img src=${element.thumbnail} class="card-img-top" alt="...">
+            <div class="card-body" style="height: 30%;">
+              <h5 class="card-title">${element.title}</h5>
+              <p class="card-text"></p>
+              <p class = "card-text">${element.brand}</p>
+              <a href="#" class="btn btn-primary">Click Here</a>
+            </div>
+        </div>`
+        });
+    }
+
+    // function to handle search and then filter
+    function searcBasedOnInput(){
+        const searchTerm = searchInput.value.toLocaleLowerCase();
+
+        let filteredData = data.products;
+
+        if(searchTerm.trim !=="") {
+            filteredData = data.products.filter((element) => {
+                if(element.title.toLocaleLowerCase().includes(searchTerm)){
+                    return element;
+                }
+                if(element.brand.toLocaleLowerCase().includes(searchTerm)){
+                    return element;
+                }
+            })
+        }
+        renderCard(filteredData);
+
+    }
+    searchInput.addEventListener('input', searcBasedOnInput);
+
+    renderCard(data.products);
+
+
 }
 
 fetchingData();
