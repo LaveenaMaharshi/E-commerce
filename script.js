e64bad09167e42afbe34c465ee0cc36ebc338db4
Taiyab <taiyab0523@gmail.com>
2023-08-11 20:35:11 +0530

    WIP: Preview part is not resolved

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
-async function fetchingData () {
-    // DOM Selector
-    const searchInput = document.querySelector('#search');
-    console.log(searchInput);
+async function getData() {
+    const response = await fetch("https://dummyjson.com/products");
 
-    //fetch request to get the for mapping the data
-    const response = await fetch('https://dummyjson.com/products');
-
-    // We are going to await for response
     const data = await response.json();
     console.log(data.products);
 
-    // Creating a logic function for rendering the cards
-    // It will be mapping filtered data
-    function renderCard(filteredData) {
-        
-        // we are going select our main body who is the parent of card structure
-        const mainBody = document.querySelector('.product-container');
-        console.log(mainBody, "This is main body.");
 
-        // 
-        mainBody.innerHTML = '';
+    function renderCard(filteredData) {
+        const mainbody = document.querySelector(".product-container");
+        console.log(mainbody, "This is main body...");
+
+        mainbody.innerHTML = "";
 
         filteredData.forEach((element) => {
-            mainBody.innerHTML += `<div class="card col-4 d-flex justify-content-between align-content-center bg-dark">
-        <img src=${element.images[0]} class="card-img-top shadow image-fluid" alt=${element.title}>
-        <div class="card-body justify-content-around" >
-            <h5 class="card-title">${element.title}</h5>
-            <p class="card-text">${element.brand}</p>
-            <a href="#" class="btn btn-primary">Add to Cart</a>
-        </div>
-    </div>`
+            mainbody.innerHTML += `<div class="product" data-name=${element.id}>
+                 <img src=${element.thumbnail} alt="">
+                 <h3>${element.title}</h3>
+                 <h5>${element.brand}</h5>
+                 <div class="price">₹ ${element.price}</div>
+             </div>`
         });
     }
 
-    // function to handle search and then filter
-    function searcBasedOnInput(){
-        const searchTerm = searchInput.value.toLocaleLowerCase();
+    renderCard(data.products);
 
-        let filteredData = data.products;
+    let previewContainer = document.querySelector(".products-preview");
 
-        if(searchTerm.trim !=="") {
-            filteredData = data.products.filter((element) => {
-                if(element.title.toLocaleLowerCase().includes(searchTerm)){
-                    return element;
-                }
-                if(element.brand.toLocaleLowerCase().includes(searchTerm)){
-                    return element;
+    let previewBox = document.querySelector(".preview");
+
+    document.querySelectorAll(".product-container .product").forEach(product => {
+        product.onclick = () => {
+            previewContainer.style.display = "flex";
+            let id = product.getAttribute("data-name");
+            previewBox.forEach(preview => {
+                let target = preview.getAttribute("data-target");
+                if (id == target) {
+                    preview.classList.add("active");
                 }
             })
         }
-        renderCard(filteredData);
+    });
 
-    }
-    // searchInput.addEventListener('input', searcBasedOnInput);
-
-    renderCard(data.products);
-
+    // previewBox.forEach(close => {
+    //     close.querySelector(".fa-times").onclick = () => {
+    //         close.classList.remove("active");
+    //         previewContainer.style.display = "none";
+    //     };
+    // });
 
 }
+getData();
 
-fetchingData();
+previewBox.forEach(close => {
+    close.querySelector(".fa-times").onclick = () => {
+        close.classList.remove("active");
+        previewContainer.style.display = "none";
+    };
+});
+
